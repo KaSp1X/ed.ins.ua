49ba55f4d0fde19533b0931f01cb01c674a9f5fe
Roman Koliadenko <romankasp1x@gmail.com>
2022-11-14 19:57:37 +0200

    Add full info page to Flask framework

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from institute import *
+
 app = Flask(__name__)
 
 @app.route("/")
 def Home():
     return ""
+
+@app.route("/<string:instituteURI>/")
+def FullInfo(instituteURI):
+    s = Institute(instituteURI)
+    return str(s.getRelations())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
