f04b79eb86e5285fee2e40ca4ab7f05013e16a12
Roman Koliadenko <romankasp1x@gmail.com>
2022-12-05 14:49:56 +0200

    Add main page view

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from model import MainPageModel
+from institute import Institute
+
 app = Flask(__name__)
 
 @app.route("/")
 def Home():
-    return ""
+    mpModel = MainPageModel()
+    institutes = [Institute(item['institute']['value'], 
+                            item['instituteName']['value'], 
+                            item['cityName']['value']) for item in mpModel.get_institutes()]
+    # print(len(institutes))
+    return render_template('home.html', institutes=institutes)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
